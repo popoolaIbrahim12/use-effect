b54658de59c1fe4c089229e527ad2addd765f9b6
popoolaIbrahim12 <ibrahimpopoola926@gmail.com>
2026-02-13 10:23:32 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,10 @@ const App = () => {
        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-6 gap-6">
         {product.map((item) => (
           
-          <div key={item.id} className="border-2 border-gray-300 rounded-lg p-4 m-4">
-            <img src={item.image} alt={item.title} className="h-40 object-contain mx-auto" />
-            <h2 className="text-lg font-semibold mt-2">{item.title}</h2>
-            <h5 className="text-sm mt-2">{item.description}</h5>
+          <div key={item.id} className="border-2 border-gray-300 rounded-lg p-4 m-4 ">
+            <img src={item.image} alt={item.title} className="h-40 object-contain mx-auto cursor-pointer" />
+            <h2 className="text-lg font-semibold mt-10">{item.title}</h2>
+            <h5 className="text-sm mt-3">{item.description}</h5>
             <p className="text-xl font-bold mt-2">${item.price}</p>
           </div>
         ))}
